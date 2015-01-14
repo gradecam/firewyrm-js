dca40f7e2f5d3d3c81daab6965eccfdc7ad31616
Robert Porter <rob@gradecam.com>
2015-01-13 21:20:55 -0700

    Initial Commit

---
+++ src/firewyrm.js
@@ -0,0 +1,5 @@
+/* globals angular */
+function blah() {
+    console.log("BLAH!");
+}
+
---
+++ test/firewyrm.spec.js
@@ -0,0 +1,10 @@
+/* globals jasmine, beforeEach, afterEach, describe, it, expect, module, inject, angular */
+'use strict';
+
+// Unit tests for fastRepeat directive. Note that these tests should always pass for both
+// fast-repeat and ng-repeat, as they should be compatible.
+describe("firewyrm", function() {
+    it("should do blah!", function() {
+        blah();
+    });
+});
