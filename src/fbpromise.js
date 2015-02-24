0e3881ff0f96a8c68b25a28abd596c135d642276
Richard Bateman <taxilian@gmail.com>
2015-02-24 13:02:04 -0800

    Update to use amdefine

--- src/fbpromise.js
+++ src/fbpromise.js
@@ -1,129 +1,132 @@
-function isObject(obj) { var type = typeof obj; return type === 'function' || type === 'object' && !!obj; }
-var toString = Object.prototype.toString;
-// Shamelessly borrowed from underscore.js
-var isFunction = function (obj) { return toString.call(obj) === '[object Function]'; };
-// Optimize `isFunction` if appropriate. Work around an IE 11 bug. Pulled from underscore.js
-if (typeof /./ !== 'function') {
-    isFunction = function(obj) { return typeof obj == 'function' || false; };
-}
-function defer(fn, arg) { setTimeout(function() { fn(arg); }, 0); }
-
-var STATES = { PEND: 1, PROMISE: 2, RESOLVE: 3, REJECT: 4 };
-
-function DeferredObject() {
-    var self=this;
-    var fulfillHandlers = [];
-    var rejectHandlers = [];
-    var state = STATES.PEND;
-    var value;
-
-    self.promise = {
-        then: function(onFulfilled, onRejected) {
-            var promise2 = new DeferredObject();
-            function makeCallback(cbFunc, type) {
-                // Return a wrapper for the provided callback function
-                return function handleCallback(value) {
-                    try {
-                        // If it's not a function, we still need to pass it through to promise2
-                        if (isFunction(cbFunc)) {
-                            var x = cbFunc(value);
-                            promise2.resolve(x);
-                        } else {
-                            promise2[type](value);
-                        }
-                    } catch (e) {
-                        // In case of exception, reject
-                        promise2.reject(e);
-                    }
-                };
-            }
-            if (state === STATES.RESOLVE) {
-                defer(function() { makeCallback(onFulfilled, "resolve")(value); });
-            } else if (state === STATES.REJECT) {
-                defer(function() { makeCallback(onRejected, "reject")(value); });
-            } else {
-                fulfillHandlers.push(makeCallback(onFulfilled, "resolve"));
-                rejectHandlers.push(makeCallback(onRejected, "reject"));
-            }
-            return promise2.promise;
-        }
-    };
-
-    function getThen(x) {
-        try {
-            return isObject(x) ? x.then : null;
-        } catch (e) {
-            self.reject(e);
-            return false;
-        }
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
+define([], function() {
+    function isObject(obj) { var type = typeof obj; return type === 'function' || type === 'object' && !!obj; }
+    var toString = Object.prototype.toString;
+    // Shamelessly borrowed from underscore.js
+    var isFunction = function (obj) { return toString.call(obj) === '[object Function]'; };
+    // Optimize `isFunction` if appropriate. Work around an IE 11 bug. Pulled from underscore.js
+    if (typeof /./ !== 'function') {
+        isFunction = function(obj) { return typeof obj == 'function' || false; };
     }
-    self.resolve = function (x, force) {
-        // Check for invalid calls
-        if (state === STATES.RESOLVE || state === STATES.REJECT) { return self; }
-        else if (x === self || x === self.promise) {
-            return self.reject(new TypeError());
-        } else if (state !== STATES.PEND && !force) {
-            return self; // ignore multiple calls
-        }
+    function defer(fn, arg) { setTimeout(function() { fn(arg); }, 0); }
 
-        // If getting then throws, getThen will call reject
-        var then = getThen(x);
-        if (then === false) { return self; }
+    var STATES = { PEND: 1, PROMISE: 2, RESOLVE: 3, REJECT: 4 };
 
-        // If then is a function, treat it accordingly
-        if (isFunction(then)) {
-            var scope_settled = false;
-            var resolve = function(x) {
-                if (scope_settled) { return self; }
-                scope_settled = true;
-                self.resolve(x, true);
-            }, reject = function(x) {
-                if (scope_settled) { return self; }
-                scope_settled = true;
-                self.reject(x, true);
-            };
+    function DeferredObject() {
+        var self=this;
+        var fulfillHandlers = [];
+        var rejectHandlers = [];
+        var state = STATES.PEND;
+        var value;
+
+        self.promise = {
+            then: function(onFulfilled, onRejected) {
+                var promise2 = new DeferredObject();
+                function makeCallback(cbFunc, type) {
+                    // Return a wrapper for the provided callback function
+                    return function handleCallback(value) {
+                        try {
+                            // If it's not a function, we still need to pass it through to promise2
+                            if (isFunction(cbFunc)) {
+                                var x = cbFunc(value);
+                                promise2.resolve(x);
+                            } else {
+                                promise2[type](value);
+                            }
+                        } catch (e) {
+                            // In case of exception, reject
+                            promise2.reject(e);
+                        }
+                    };
+                }
+                if (state === STATES.RESOLVE) {
+                    defer(function() { makeCallback(onFulfilled, "resolve")(value); });
+                } else if (state === STATES.REJECT) {
+                    defer(function() { makeCallback(onRejected, "reject")(value); });
+                } else {
+                    fulfillHandlers.push(makeCallback(onFulfilled, "resolve"));
+                    rejectHandlers.push(makeCallback(onRejected, "reject"));
+                }
+                return promise2.promise;
+            }
+        };
+
+        function getThen(x) {
             try {
-                state = STATES.PROMISE;
-                then.call(x, resolve, reject);
+                return isObject(x) ? x.then : null;
             } catch (e) {
-                if (!scope_settled) {
-                    state = STATES.PEND;
-                    self.reject(e);
+                self.reject(e);
+                return false;
+            }
+        }
+        self.resolve = function (x, force) {
+            // Check for invalid calls
+            if (state === STATES.RESOLVE || state === STATES.REJECT) { return self; }
+            else if (x === self || x === self.promise) {
+                return self.reject(new TypeError());
+            } else if (state !== STATES.PEND && !force) {
+                return self; // ignore multiple calls
+            }
+
+            // If getting then throws, getThen will call reject
+            var then = getThen(x);
+            if (then === false) { return self; }
+
+            // If then is a function, treat it accordingly
+            if (isFunction(then)) {
+                var scope_settled = false;
+                var resolve = function(x) {
+                    if (scope_settled) { return self; }
+                    scope_settled = true;
+                    self.resolve(x, true);
+                }, reject = function(x) {
+                    if (scope_settled) { return self; }
+                    scope_settled = true;
+                    self.reject(x, true);
+                };
+                try {
+                    state = STATES.PROMISE;
+                    then.call(x, resolve, reject);
+                } catch (e) {
+                    if (!scope_settled) {
+                        state = STATES.PEND;
+                        self.reject(e);
+                    }
+                }
+            } else {
+                // For any other value, do a normal resolve
+                value = x;
+                state = STATES.RESOLVE;
+                for (var i = 0; i < fulfillHandlers.length; ++i) {
+                    defer(fulfillHandlers[i], value);
                 }
             }
-        } else {
-            // For any other value, do a normal resolve
-            value = x;
-            state = STATES.RESOLVE;
-            for (var i = 0; i < fulfillHandlers.length; ++i) {
-                defer(fulfillHandlers[i], value);
+        };
+        self.reject = function(x) {
+            // Ignore the call if we're already settled, otherwise reject
+            if (state === STATES.RESOLVE || state === STATES.REJECT) { return self; }
+            else if (x === self || x === self.promise) {
+                return self.reject(new TypeError());
             }
-        }
-    };
-    self.reject = function(x) {
-        // Ignore the call if we're already settled, otherwise reject
-        if (state === STATES.RESOLVE || state === STATES.REJECT) { return self; }
-        else if (x === self || x === self.promise) {
-            return self.reject(new TypeError());
-        }
-        value = x;
-        state = STATES.REJECT;
-        for (var i = 0; i < rejectHandlers.length; ++i) {
-            defer(rejectHandlers[i], value);
-        }
-        return self;
-    };
-}
-function makeDeferred() {
-    var dfd = new DeferredObject();
-    if (arguments.length > 0) {
-        dfd.resolve(arguments[1]);
+            value = x;
+            state = STATES.REJECT;
+            for (var i = 0; i < rejectHandlers.length; ++i) {
+                defer(rejectHandlers[i], value);
+            }
+            return self;
+        };
     }
-    return dfd;
-}
-makeDeferred.resolved = makeDeferred;
-makeDeferred.rejected = function(err) {
-    return makeDeferred().reject(err);
-};
+    function makeDeferred() {
+        var dfd = new DeferredObject();
+        if (arguments.length > 0) {
+            dfd.resolve(arguments[1]);
+        }
+        return dfd;
+    }
+    makeDeferred.resolved = makeDeferred;
+    makeDeferred.rejected = function(err) {
+        return makeDeferred().reject(err);
+    };
 
-module.exports = makeDeferred;
+    return makeDeferred;
+});
--- src/firewyrm.js
+++ src/firewyrm.js
@@ -1,93 +1,94 @@
-/* globals angular */
-
-var Deferred = require('./fbpromise');
-
-var ConsoleWyrmHole = function() {
-    var msgIdCnt = 0;
-    var dfds = {};
-
-    this.sendMessage = function(msg, data) {
-        var msgObj = {
-            message: msg,
-            messageId: ++msgIdCnt,
-            data: data
-        };
-        console.log('sendMessage: ', msgObj);
-        var dfd = new Deferred();
-        dfds[msgIdCnt] = dfd;
-        return dfd.promise;
-    };
-};
-
-
-var objectIdWeakMap = new WeakMap();
-var objectIdMap = {};
-
-function serializeValue(value) {
-    if (typeof value !== 'object') { return value; } // Only objects get translated
-    if(!objectIdWeakMap.has(value)) {
-        var id = Number(('' + Math.random()).slice(2));
-        objectIdMap[id] = value;
-        objectIdWeakMap.set(value, id);
+/*globals WeakMap*/
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
+define(['./fbpromise'], function(Deferred) {
+    function blah() {
+        console.log("BLAH!");
     }
-    return objectIdWeakMap.get(value);
-}
-function deserializeValue(value) {
-    if (typeof value !== 'object') { return value; } // Only objects get translated
-    return objectIdMap[value.objectId] || value;
-}
 
+    var ConsoleWyrmHole = function() {
+        var msgIdCnt = 0;
+        var dfds = {};
 
-var WyrmJSAPI = function(objectId, fnList, pList, name, wyrmHole) {
-    var self = this;
-
-    self.objectId = objectId || 0;
-    self.fnList = fnList || [];
-    self.pList = pList || [];
-    self.name = name || '';
-
-    self.toString = function() {
-        return "[WyrmJSAPI Object: " + this.name + "]";
+        this.sendMessage = function(msg, data) {
+            var msgObj = {
+                message: msg,
+                messageId: ++msgIdCnt,
+                data: data
+            };
+            console.log('sendMessage: ', msgObj);
+            var dfd = new Deferred();
+            dfds[msgIdCnt] = dfd;
+            return dfd.promise;
+        };
     };
 
-    self.fnList.forEach(function(fnName) {
-        self[fnName] = function() {
-            var args = Array.prototype.map.call(arguments, function(arg) { return serializeValue(arg); });
-            return wyrmHole.sendMessage('CallFn', {
-                objectId: self.objectId,
-                fnName: fnName,
-                args: arguments
-            });
+    var objectIdWeakMap = new WeakMap();
+    var objectIdMap = {};
+
+    function serializeValue(value) {
+        if (typeof value !== 'object') { return value; } // Only objects get translated
+        if(!objectIdWeakMap.has(value)) {
+            var id = Number(('' + Math.random()).slice(2));
+            objectIdMap[id] = value;
+            objectIdWeakMap.set(value, id);
+        }
+        return objectIdWeakMap.get(value);
+    }
+    function deserializeValue(value) {
+        if (typeof value !== 'object') { return value; } // Only objects get translated
+        return objectIdMap[value.objectId] || value;
+    }
+
+
+    var WyrmJSAPI = function(objectId, fnList, pList, name, wyrmHole) {
+        var self = this;
+
+        self.objectId = objectId || 0;
+        self.fnList = fnList || [];
+        self.pList = pList || [];
+        self.name = name || '';
+
+        self.toString = function() {
+            return "[WyrmJSAPI Object: " + this.name + "]";
         };
-    });
 
-    self.pList.forEach(function(pName) {
-        self.__defineGetter__(pName, function() {
-            return wyrmHole.sendMessage('GetP', {
-                objectId: self.objectId,
-                pName: pName
+        self.fnList.forEach(function(fnName) {
+            self[fnName] = function() {
+                var args = Array.prototype.map.call(arguments, function(arg) { return serializeValue(arg); });
+                return wyrmHole.sendMessage('CallFn', {
+                    objectId: self.objectId,
+                    fnName: fnName,
+                    args: arguments
+                });
+            };
+        });
+
+        self.pList.forEach(function(pName) {
+            self.__defineGetter__(pName, function() {
+                return wyrmHole.sendMessage('GetP', {
+                    objectId: self.objectId,
+                    pName: pName
+                });
+            });
+            self.__defineSetter__(pName, function(val) {
+                return wyrmHole.sendMessage('SetP', {
+                    objectId: self.objectId,
+                    pName: pName,
+                    value: val
+                });
             });
         });
-        self.__defineSetter__(pName, function(val) {
-            return wyrmHole.sendMessage('SetP', {
-                objectId: self.objectId,
-                pName: pName,
-                value: val
-            });
-        });
-    });
 
-};
+    };
 
 
-function getRootObject(wyrmHole) {
-    var rootAPI = new WyrmJSAPI(0, ['getRootObject'],[], 'rootAPI', wyrmHole);
-    return rootAPI.getRootObject();
-}
+    function getRootObject(wyrmHole) {
+        var rootAPI = new WyrmJSAPI(0, ['getRootObject'],[], 'rootAPI', wyrmHole);
+        return rootAPI.getRootObject();
+    }
 
-if (module) {
-    module.exports = {
+    return {
         WyrmJSAPI: WyrmJSAPI,
         ConsoleWyrmHole: ConsoleWyrmHole
     };
-}
+});
