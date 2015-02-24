c448158933517f14dc1c663743c4e0a1f8eff5e0
Ben Loveridge <benl@gradecam.com>
2015-02-24 13:13:56 -0700

    Run tests whenever src or spec files change

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,24 @@
 
 module.exports = function(grunt) {
     grunt.loadNpmTasks('jasmine');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.initConfig({ });
+    grunt.initConfig({
+        watch: {
+            tests: {
+                files: [
+                    'src/**/*.js',
+                    'spec/**/*[sS]pec.js',
+                    '!Gruntfile.js',
+                ],
+                tasks: ['specs'],
+                options: {
+                    atBegin: true,
+                },
+            }
+        }
+    });
 
-    grunt.registerTask('default', ['specs']);
+    grunt.registerTask('default', ['watch:tests']);
+    grunt.registerTask('test', ['specs']);
 };
