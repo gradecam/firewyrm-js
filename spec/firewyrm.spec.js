a8f669ccbb9f86732e002d03892f401b358762cb
Ben Loveridge <benl@gradecam.com>
2015-03-20 14:06:23 -0600

    Start with a blank slate

--- spec/firewyrm.spec.js
+++ spec/firewyrm.spec.js
@@ -5,45 +5,4 @@ var ConsoleWyrmHole = require('../src/consoleWyrmHole');
 var MockWyrmHole = require('../src/mockWyrmHole');
 
 describe("firewyrm", function() {
-    describe("WyrmJSAPI messages", function() {
-        it("should define a function when passed in the fnList argument", function() {
-            var consoleWyrmHole = new ConsoleWyrmHole();
-            var x = new fw.WyrmJSAPI(0, ['blahFunc'], [], 'TestAPI', consoleWyrmHole);
-            x.blahFunc();
-        });
-
-        var mockWyrmHole = new MockWyrmHole();
-
-        it("should create a properly formed Invoke message", function() {
-            var x = new fw.WyrmJSAPI(1, ['blahFunc'], [], 'TestAPI', mockWyrmHole);
-            x.blahFunc();
-            console.log(JSON.stringify(mockWyrmHole.lastMessage.args));
-            expect(mockWyrmHole.lastMessage.args).toEqual(['Invoke', 1, 'blahFunc', []]);
-        });
-
-        it("set property should be well formed.", function() {
-            var myObj = new fw.WyrmJSAPI(1, [], ['x'], 'TestAPI', mockWyrmHole);
-            myObj.x = 1;
-            console.log(JSON.stringify(mockWyrmHole.lastMessage.args));
-            expect(mockWyrmHole.lastMessage.args).toEqual(['SetP', 1, 'x', 1]);
-        });
-
-        it("get property should be well formed.", function() {
-            var myObj = new fw.WyrmJSAPI(1, [], ['x'], 'TestAPI', mockWyrmHole);
-            var myVal = myObj.x;
-            console.log(JSON.stringify(mockWyrmHole.lastMessage.args));
-            expect(mockWyrmHole.lastMessage.args).toEqual(['GetP', 1, 'x']);
-        });
-
-    });
-
-    describe("getRootObject", function() {
-        it("should create a properly formed DescribeObj message", function() {
-            var mockWyrmHole = new MockWyrmHole();
-            fw.getRootObject(mockWyrmHole);
-            expect(mockWyrmHole.lastMessage.args).toEqual([ 'DescribeObj', 0 ]);
-        });
-    });
-
-
 });
--- src/firewyrm.js
+++ src/firewyrm.js
@@ -1,62 +1,7 @@
 /*globals WeakMap*/
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(['./fbpromise', './consoleWyrmHole', './mockWyrmHole'], function(Deferred, ConsoleWyrmHole, MockWyrmHole) {
-
-    var objectIdWeakMap = new WeakMap();
-    var objectIdMap = {};
-
-    function serializeValue(value) {
-        if (typeof value !== 'object') { return value; } // Only objects get translated
-        if(!objectIdWeakMap.has(value)) {
-            var id = Number(('' + Math.random()).slice(2));
-            objectIdMap[id] = value;
-            objectIdWeakMap.set(value, id);
-        }
-        return objectIdWeakMap.get(value);
-    }
-    function deserializeValue(value) {
-        if (typeof value !== 'object') { return value; } // Only objects get translated
-        return objectIdMap[value.objectId] || value;
-    }
-
-
-    var WyrmJSAPI = function(objectId, fnList, pList, name, wyrmHole) {
-        var self = this;
-
-        self.objectId = objectId || 0;
-        self.fnList = fnList || [];
-        self.pList = pList || [];
-        self.name = name || '';
-
-        self.toString = function() {
-            return "[WyrmJSAPI Object: " + this.name + "]";
-        };
-
-        self.fnList.forEach(function(fnName) {
-            self[fnName] = function() {
-                var args = Array.prototype.map.call(arguments, function(arg) { return serializeValue(arg); });
-                return wyrmHole.sendMessage(['Invoke', self.objectId, fnName, args]);
-            };
-        });
-
-        self.pList.forEach(function(pName) {
-            self.__defineGetter__(pName, function() {
-                return wyrmHole.sendMessage(['GetP', objectId, pName]);
-            });
-            self.__defineSetter__(pName, function(val) {
-                return wyrmHole.sendMessage(['SetP', objectId, pName, val]);
-            });
-        });
-
-    };
-
-
-    function getRootObject(wyrmHole) {
-        return wyrmHole.sendMessage(['DescribeObj', 0]);
-    }
-
     return {
-        WyrmJSAPI: WyrmJSAPI,
-        getRootObject: getRootObject
+        create: function() {}
     };
 });
