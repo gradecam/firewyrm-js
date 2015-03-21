697bc9f2eb77d101408ba104c2380d27b50e6102
Ben Loveridge <benl@gradecam.com>
2015-03-21 00:08:58 -0600

    Add tests to make sure the queenling sets up all the correct properties

--- spec/firewyrm.spec.js
+++ spec/firewyrm.spec.js
@@ -1,9 +1,10 @@
 /* globals jasmine, beforeEach, afterEach, describe, it, expect */
 'use strict';
+var _ = require('underscore');
+var clockHelpers = require('./helpers/clock');
 var fw = require('../src/firewyrm');
 var MockWyrmHole = require('../src/mockWyrmHole');
 //var ConsoleWyrmHole = require('../src/consoleWyrmHole');
-var clockHelpers = require('./helpers/clock');
 
 describe("firewyrm", function() {
     beforeEach(function() {
@@ -38,6 +39,29 @@ describe("firewyrm", function() {
             mockWyrmHole.lastMessage.respond('success', mockWyrmHole.spawnId); // respond to "New"
             mockWyrmHole.lastMessage.respond('success', enumProps); // respond to "Enum"
             expect(queenling).toBeResolved();
+        });
+        describe("after the queenling is resolved", function() {
+            beforeEach(function() {
+                mockWyrmHole.lastMessage.respond('success', mockWyrmHole.spawnId); // respond to "New"
+                mockWyrmHole.lastMessage.respond('success', enumProps); // respond to "Enum"
+                queenling.then(function(alienWyrmling) {
+                    queenling = alienWyrmling; // this is the final resolved AlienWyrmling from initial contact
+                });
+                jasmine.clock().flush(); // make sure any promises are resolved
+            });
+            it("should make note of its spawnId and objectId", function() {
+                expect(queenling.spawnId).toBe(mockWyrmHole.spawnId);
+                expect(queenling.objectId).toBe(0);
+            });
+            it("should have a generic property getter and setter", function() {
+                expect(queenling.getProperty).toEqual(jasmine.any(Function));
+                expect(queenling.setProperty).toEqual(jasmine.any(Function));
+            });
+            it("should have properties for each of the items received from Enum", function() {
+                _.each(enumProps, function(prop) {
+                    expect(queenling.hasOwnProperty(prop)).toBe(true);
+                });
+            });
         });
     });
 });
